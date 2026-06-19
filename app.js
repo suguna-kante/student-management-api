056642eaaaeb8f2299baf47f98b97dc35f0fb1fc
suguna-kante <kantesuguna80@gmail.com>
2026-06-19 12:22:18 +0530

    Added Render port configuration

--- app.js
+++ app.js
@@ -41,9 +41,11 @@ app.use(errorHandler);
 
 
 // MongoDB Connection
-mongoose.connect("mongodb://127.0.0.1:27017/collegeDb")
+
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
-app.listen(5000, () => {
-    console.log("Server Running");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
 });
